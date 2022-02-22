add42ff3c97731a5f591acaf9f10574ca29f7d41
MdHamidUddin <hamiduddin2018@gmail.com>
2022-02-22 17:33:07 +0600

    First Commit

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AskNLearn.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+
+        }
+
+       // [HttpPost]
+        //public ActionResult Login()
+        //{
+
+        //    return View();
+        //}
+    }
+}
