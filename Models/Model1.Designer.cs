03926a612933e548cbf91f339af6bf60dfcef334
MdHamidUddin <hamiduddin2018@gmail.com>
2022-02-24 19:28:15 +0600

    Login Page Updated

--- Models/Model1.Designer.cs
+++ Models/Model1.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\Study Zone\Ask & Learn\Models\Model1.edmx'. 
+﻿// T4 code generation is enabled for model 'E:\Books & Notes\10th Semester\Advanced Programming With .Net\Mid_Project\AskNLearn\Models\Model1.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
