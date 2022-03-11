a8d2fef7e613f07cc49d166828df26302966e595
JisunAurnob <jisunabedin@gmail.com>
2022-03-11 23:00:20 +0600

    Adding Documents and previewing details done

--- Models/Instructor/DocumentsModel.cs
+++ Models/Instructor/DocumentsModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,6 +9,7 @@ namespace AskNLearn.Models.Instructor
     public class DocumentsModel
     {
         public int did { get; set; }
+        [Required]
         public int coid { get; set; }
         public string image { get; set; }
         public string videoLink { get; set; }
