7752c369926d692d7fe737be97fe928bd3a80773
JisunAurnob <jisunabedin@gmail.com>
2022-03-13 03:24:32 +0600

    Quiz & Enrolled Students Added

---
+++ Models/Instructor/EnrolledListModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AskNLearn.Models.Instructor
+{
+    public class EnrolledListModel
+    {
+        public int eid { get; set; }
+        public string UserName { get; set; }
+        public string courseTitle { get; set; }
+        public DateTime dateTime { get; set; }
+    }
+}
